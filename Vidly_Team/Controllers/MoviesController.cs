ebaf320ebce95acb5aa0e9794b9a1360b7f4cce0
DESKTOP-0R41BDV\Riad133 <riad.nazrul@gmail.com>
2017-03-26 14:28:02 +0600

    Add Movie class and controller

---
+++ Vidly_Team/Controllers/MoviesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly_Team.Models;
+
+namespace Vidly_Team.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movie(){Name = "Shrink"};
+        
+            return View(movie);
+        }
+    }
+}
