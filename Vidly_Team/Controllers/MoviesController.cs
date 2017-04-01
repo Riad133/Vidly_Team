267d55a0e78fda0c96d4a1669828308af5699155
DESKTOP-0R41BDV\Riad133 <riad.nazrul@gmail.com>
2017-04-01 22:12:57 +0600

    Revert "Commit marge"
    This reverts commit 49c05d2e33d07fd67cbd81f1c224a7aaecf065a1.

--- Vidly_Team/Controllers/MoviesController.cs
+++ Vidly_Team/Controllers/MoviesController.cs
@@ -9,7 +9,6 @@ namespace Vidly_Team.Controllers
 {
     public class MoviesController : Controller
     {
-<<<<<<< HEAD
         // GET: Movies
         public ActionResult Random()
         {
@@ -18,14 +17,4 @@ namespace Vidly_Team.Controllers
             return View(movie);
         }
     }
-=======
-        //
-        // GET: /Movies/
-        public ActionResult Random()
-        {
-            var movie = new Movie() { Name = "Terminator" };
-            return View(movie);
-        }
-	}
->>>>>>> 737ef40b0ade6b4bbd7a830145bf079af011813e
 }
