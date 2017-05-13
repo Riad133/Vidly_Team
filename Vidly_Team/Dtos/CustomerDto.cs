9cc102ca52d64cd0d1dabe9e20b7d44793c325c0
DESKTOP-0R41BDV\Riad133 <riad.nazrul@gmail.com>
2017-05-13 21:02:05 +0600

    Add Customer DTO

---
+++ Vidly_Team/Dtos/CustomerDto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Vidly_Team.Models;
+
+namespace Vidly_Team.Dtos
+{
+    public class CustomerDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+
+        public string Name { get; set; }
+        
+        [Min18YearsIfMember]
+        public DateTime? BirthDate { get; set; }
+
+        public bool IsSubscribeToNewsletter { get; set; }
+
+        public byte MembershipTypeId { get; set; }
+    }
+}
